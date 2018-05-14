a956dabe141b363745d59533772ab7e434633c8e
John O'Connell <john.oconnell@gmail.com>
2018-05-14 11:49:06 -0700

    new: dev: Connector can see seals [!minor]

--- src/main/java/jco2641/thaumcomp/items/SealConnector.java
+++ src/main/java/jco2641/thaumcomp/items/SealConnector.java
@@ -6,8 +6,9 @@ import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import thaumcraft.api.golems.ISealDisplayer;
 
-public class SealConnector extends Item {
+public class SealConnector extends Item implements ISealDisplayer {
     public SealConnector() {
         setRegistryName("sealconnector");
         setUnlocalizedName(Reference.MODID + ".sealconnector");
